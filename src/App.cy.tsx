15e09b4c402146f34aa0ae1163ce8677e388f6a2
Lab 2 <matheusdanielgomess@gmail.com>
2023-02-23 20:17:14 -0400

    Added component test

---
+++ src/App.cy.tsx
@@ -0,0 +1,8 @@
+import App from './App';
+
+describe('<App />', () => {
+  it('should render a h1 title with an welcome message', () => {
+    cy.mount(<App />);
+    cy.get('h1').should('have.text', 'Oi!');
+  });
+});
